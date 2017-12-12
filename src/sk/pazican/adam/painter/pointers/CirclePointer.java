73725c27d097d6e59607dd2a21694d7ccd61a536
Adam Pažičan <ado99881@gmail.com>
2017-12-12 21:52:14 +0100

    CirclePointer documented

--- src/sk/pazican/adam/painter/pointers/CirclePointer.java
+++ src/sk/pazican/adam/painter/pointers/CirclePointer.java
@@ -3,25 +3,53 @@ package sk.pazican.adam.painter.pointers;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 
+/**
+ * Trieda reprezentuje kruhový kurzor pre kresliacu plochu
+ *
+ * @author Adam Pažičan
+ * @version 2017.12.12
+ */
 public class CirclePointer extends Circle implements IPointer {
 
+    /**
+     * Konštruktor nastavuje defaultné nastavenia kurzora
+     *
+     * @param radius rádius kurzora
+     */
     public CirclePointer(int radius) {
         super(radius / 2);
         this.setFill(Color.TRANSPARENT);
         this.setStroke(Color.BLACK);
         this.setStrokeWidth(1);
-        this.setManaged(false);
+        this.setManaged(false); // node je vylúčený z layoutových kalkulácií rodiča
     }
 
+    /**
+     * Zmení veľkosť kurzora
+     *
+     * @param radius nový radius kurzora
+     * @param y v tejto triede nerobi nič ale musí byť kvôli kompatibilite s interfaceom IPointer
+     */
+    @Override
     public void setSize(int radius, int y) {
         this.setRadius(radius / 2);
     }
 
+    /**
+     * Zmení súradnicu x kurzora
+     *
+     * @param x nové x kurzora
+     */
     @Override
     public void setX(double x) {
         this.setCenterX(x + this.getRadius());
     }
 
+    /**
+     * Zmení súradnicu y kurzora
+     *
+     * @param y nové y kurzora
+     */
     @Override
     public void setY(double y) {
         this.setCenterY(y + this.getRadius());
