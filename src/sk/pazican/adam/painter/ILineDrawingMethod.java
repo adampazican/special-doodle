45ca53535ef311557b20bde7cadad630197aae3e
Adam Pažičan <ado99881@gmail.com>
2017-12-12 22:08:44 +0100

    Interfaces documented

--- src/sk/pazican/adam/painter/ILineDrawingMethod.java
+++ src/sk/pazican/adam/painter/ILineDrawingMethod.java
@@ -1,5 +1,13 @@
 package sk.pazican.adam.painter;
 
+/**
+ * Funkcionalny interface obsahujúci len 1 metódu.
+ * Tento interface nam dovoľuje vytvárať lambda funkcie a ďalej ich posúvať funkciám.
+ *
+ * @author Adam Pažičan
+ * @version 2017.12.12
+ */
+@FunctionalInterface
 interface ILineDrawingMethod {
     void drawLine(int x, int y, int width, int height);
 }
--- src/sk/pazican/adam/painter/pointers/IPointer.java
+++ src/sk/pazican/adam/painter/pointers/IPointer.java
@@ -1,6 +1,11 @@
 package sk.pazican.adam.painter.pointers;
 
-
+/**
+ * Interface opisujúci metódy kurzora.
+ *
+ * @author Adam Pažičan
+ * @version 2017.12.12
+ */
 public interface IPointer {
     void setSize(int width, int height);
     void setX(double x);
