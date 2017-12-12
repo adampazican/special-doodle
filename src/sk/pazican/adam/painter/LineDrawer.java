11ecda81ae32bea9ef854a6252ef56e67a17a158
Adam Pažičan <ado99881@gmail.com>
2017-12-12 21:34:50 +0100

    LineDrawer class documented

--- src/sk/pazican/adam/painter/LineDrawer.java
+++ src/sk/pazican/adam/painter/LineDrawer.java
@@ -1,10 +1,34 @@
 package sk.pazican.adam.painter;
 
+/**
+ * Trieda ktorá sa stará o vykresľovanie nových čiar
+ *
+ * @author Adam Pažičan
+ * @version 2017.12.12
+ */
 public class LineDrawer {
+    /**
+     * Konvertuje argumenty z double na int
+     *
+     * @param x0 začiatočné x
+     * @param y0 začiatočné y
+     * @param x1 koncové x
+     * @param y1 koncové y
+     * @param drawLine Lambda metóda na vykreslenie jednej časti čiary (štvorec, kruh, ...)
+     */
     public void drawLine(double x0, double y0, double x1, double y1, ILineDrawingMethod drawLine) {
         this.drawLine((int)x0, (int)y0, (int)x1, (int)y1, drawLine);
     }
 
+    /**
+     * Vykresľuje čiari pomocou Bresenhamovho algoritmu.
+     *
+     * @param x0 začiatočné x
+     * @param y0 začiatočné y
+     * @param x1 koncové x
+     * @param y1 koncové y
+     * @param drawLine Lambda metóda na vykreslenie jednej časti čiary (štvorec, kruh, ...)
+     */
     public void drawLine(int x0, int y0, int x1, int y1, ILineDrawingMethod drawLine) {
         int deltaX = Math.abs(x1 - x0);
         int deltaY = -Math.abs(y1 - y0);
